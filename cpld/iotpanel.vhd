fb3fc58cb0351200a0b4510118e6164e2ca522ac
Alvaro <alvieboy@alvie.com>
2015-05-25 09:03:05 +0100

    Add PCB and CPLD design

---
+++ cpld/iotpanel.vhd
@@ -0,0 +1,158 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity iotpanel is
+  port (
+    clk:  in std_logic;
+    cs:   inout std_logic;
+    --di:   in std_logic;
+
+    rgb:  out std_logic_vector(5 downto 0);
+    col:  out std_logic_vector(3 downto 0);
+    stb:  out std_logic;
+    clko:  out std_logic;
+
+    --idtr:  in std_logic;
+    gpio13: in std_logic;
+    gpio14: in std_logic;
+    espreset: out std_logic;
+    espen:  inout std_logic;
+    esptx:  in std_logic;
+    esprx:  out std_logic;
+    gpio2: inout std_logic;
+    gpio0: inout std_logic;
+    gpio5: inout std_logic;
+    gpio4: inout std_logic;
+    --espchpd: out std_logic;
+    gpio16: inout std_logic;
+    gpio12: inout std_logic;
+    oe: inout std_logic;
+    panelen: inout std_logic;
+
+    usr:    inout std_logic_vector(6 downto 3);
+    iusr:   in std_logic
+  );
+end iotpanel;
+
+architecture Behavioral of iotpanel is
+  signal shifter: std_logic_vector(9 downto 0);
+  alias   irx: std_logic is usr(5);
+  alias   idtr:std_logic is usr(4);
+  signal internal_reset_q: std_logic;
+  signal hclock: std_logic;
+  signal data_queued: std_logic;
+  signal internalcs:  std_logic;
+  alias di: std_logic is gpio13;
+begin
+
+  -- CH_PD          Pull-up
+  -- GPIO16/RST     Pull-up
+  -- GPIO15         Pull-down
+  -- GPIO2          Pull-up
+  -- GPIO0          Pull-up for normal or pull-down for bootloader mode.
+
+  gpio2 <= 'Z'; -- External pullup
+  gpio0 <= not irx; -- RX from external programmer.
+
+  gpio16 <= 'Z'; -- External pullup
+  gpio12 <= 'Z';
+  gpio5  <= 'Z';
+
+
+  cs <= '0' when gpio16='1' else 'Z';  -- Startup/Working CS mode.
+  -- CS is now gpio4
+  internalcs <= gpio4 when gpio16='0' else '1'; -- Ignore commands when SW not running.
+
+  usr(3) <= gpio4;--'Z';
+
+  esprx <= usr(5);
+  usr(6) <= esptx;
+
+  oe <= gpio5 when gpio16='0' else '1';
+
+  panelen <= 'Z';  -- not used yet
+  espen <= 'Z';    -- not used yet
+
+  espreset<='0' when idtr='1' else 'Z';
+
+  --espen<='0' when idtr='0' and irx='1' else '1';
+
+  stb <= gpio12;
+  --stb <= clk;
+  --clko <= gpio14;
+
+
+  process(clk,internalcs)
+  begin
+    if internalcs='1' then
+      shifter <= (others => '0');
+    elsif rising_edge(clk) then
+      if internal_reset_q='1' then
+        shifter(8 downto 2) <= (others => '0');
+        shifter(1) <= shifter(0);
+        shifter(0) <= di;
+      else
+        shifter <= shifter(shifter'HIGH-1 downto 0) & di;
+      end if;
+    end if;
+  end process;
+
+  --clko <= shifter(8);
+  clko <= hclock;
+
+  process(clk,internalcs)
+  begin
+    if internalcs='1' then
+      internal_reset_q<='0';
+      data_queued<='0';
+    elsif rising_edge(clk) then
+      if internal_reset_q='1' then
+        internal_reset_q<='0';
+      else
+        internal_reset_q <= shifter(8);
+        if shifter(8)='1' then
+          data_queued<='1';
+        end if;
+      end if;
+      if hclock='1' then
+        data_queued<='0';
+      end if;
+    end if;
+  end process;
+
+  process(clk,internalcs)
+  begin
+    if internalcs='1' then
+      hclock<='0';
+    elsif rising_edge(clk) then
+      if shifter(8 downto 6)="001" then
+        hclock<='0';
+      else
+        --if internal_reset_q='1' then
+        if data_queued='1' and shifter(3)='1' and internal_reset_q='0' then
+          hclock <= '1';
+        end if;
+      end if;
+    end if;
+  end process;
+
+  process(clk)
+  begin
+    if rising_edge(clk) then
+      if shifter(8 downto 6)="001" then
+        rgb <= shifter(4 downto 0) & di;
+      end if;
+    end if;
+  end process;
+
+  process(gpio4)
+  begin
+    if rising_edge(gpio4) then
+      if shifter(8)='0' then
+        col <= shifter(3 downto 0);
+      end if;
+    end if;
+  end process;
+
+end Behavioral;
+
---
+++ cpld/tb.vhd
@@ -0,0 +1,153 @@
+--------------------------------------------------------------------------------
+-- Company: 
+-- Engineer:
+--
+-- Create Date:   11:05:08 03/28/2015
+-- Design Name:   
+-- Module Name:   /home/alvieboy/otpanel/cpld/tb.vhd
+-- Project Name:  cpld
+-- Target Device:  
+-- Tool versions:  
+-- Description:   
+-- 
+-- VHDL Test Bench Created by ISE for module: top
+-- 
+-- Dependencies:
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+--
+-- Notes: 
+-- This testbench has been automatically generated using types std_logic and
+-- std_logic_vector for the ports of the unit under test.  Xilinx recommends
+-- that these types always be used for the top-level I/O of a design in order
+-- to guarantee that the testbench will bind correctly to the post-implementation 
+-- simulation model.
+--------------------------------------------------------------------------------
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+ 
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--USE ieee.numeric_std.ALL;
+library work;
+use work.all;
+
+ENTITY tb IS
+END tb;
+ 
+ARCHITECTURE behavior OF tb IS 
+ 
+    -- Component Declaration for the Unit Under Test (UUT)
+
+   --Inputs
+   signal clk : std_logic := '0';
+   signal cs : std_logic := '0';
+   signal di : std_logic := '0';
+   --signal idtr : std_logic := '0';
+   signal gpio13 : std_logic := '0';
+   signal gpio14 : std_logic := '0';
+   signal esptx : std_logic := '0';
+
+	--BiDirs
+   signal gpio2 : std_logic;
+   signal gpio0 : std_logic;
+   signal gpio5 : std_logic;
+   signal gpio4 : std_logic;
+   signal gpio16 : std_logic;
+   signal gpio12 : std_logic;
+   signal usr : std_logic_vector(6 downto 3);
+
+ 	--Outputs
+   signal rgb : std_logic_vector(5 downto 0);
+   signal col : std_logic_vector(3 downto 0);
+   signal stb : std_logic;
+   signal clko : std_logic;
+   signal espreset : std_logic;
+   signal espen : std_logic;
+   signal esprx : std_logic;
+   --signal espchpd : std_logic;
+   signal oe : std_logic;
+   signal panelen : std_logic;
+
+   -- Clock period definitions
+   constant clk_period : time := 10 ns;
+--   constant clko_period : time := 10 ns;
+
+   alias   idtr:std_logic is usr(4);
+
+BEGIN
+
+  usr(4) <= '1';
+  gpio16 <= '0';
+
+	-- Instantiate the Unit Under Test (UUT)
+   uut: entity work.iotpanel PORT MAP (
+          clk => clk,
+          cs => cs,
+          --di => di,
+          rgb => rgb,
+          col => col,
+          stb => stb,
+          clko => clko,
+          --idtr => idtr,
+          gpio13 => gpio13,
+          gpio14 => gpio14,
+          espreset => espreset,
+          espen => espen,
+          esptx => esptx,
+          esprx => esprx,
+          gpio2 => gpio2,
+          gpio0 => gpio0,
+          gpio5 => gpio5,
+          gpio4 => gpio4,
+          --espchpd => espchpd,
+          gpio16 => gpio16,
+          gpio12 => di,--gpio12,
+          oe => oe,
+          panelen => panelen,
+          usr => usr,
+          iusr => 'X'
+        );
+
+   -- Stimulus process
+   stim_proc: process
+    procedure transfer(data: in std_logic_vector; deassert: in boolean) is
+      variable w,i: natural;
+      variable d: std_logic_vector(data'high downto data'low);
+    begin
+      w := data'length;
+      d := data;
+      wait for clk_period/2;
+      cs <= '0';
+      wait for clk_period/2;
+      l1: for i in 0 to w-1 loop
+        -- Setup data.
+        di <= d(data'high);
+        wait for clk_period/2;
+        clk <= '1';
+        wait for clk_period/2;
+        clk <= '0';
+        d := d(data'high-1 downto 0) & 'X';
+      end loop;
+      wait for clk_period/2;
+      if (deassert) then cs<='1'; end if;
+    end procedure;
+   begin		
+      -- hold reset state for 100 ns.
+      cs <= '1';
+      wait for 100 ns;	
+
+      transfer("11" & "010"&"010"&'X', false);
+      transfer("11" & "111"&"111"&'X', false);
+      transfer("11" & "000"&"000"&'X', false);
+      transfer("10" & "XXX"&"XXX"&'X', true);
+
+      transfer("00" & "0001111", true);
+
+      wait for 100 ns;
+      wait;
+   end process;
+
+END;
