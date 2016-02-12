e91150394306838127f8eb6a5f932acc1df3776e
Alvaro <alvieboy@alvie.com>
2016-02-12 09:37:03 +0000

    New flash infrasctructure

---
+++ firmware/esp8266/user/flash.c
@@ -0,0 +1,103 @@
+#include "flash.h"
+#include "osapi.h"
+#include "protos.h"
+#include "alloc.h"
+
+#define DEBUGFLASH(x...) /* os_printf(x) */
+
+void ICACHE_FLASH_ATTR flash_control_init(flash_control_t *ctrl)
+{
+    ctrl->cached_sector_number = -1;
+    ctrl->cached_sector = os_malloc(SECTORSIZE);
+    ctrl->cached_dirty = 0;
+}
+
+void ICACHE_FLASH_ATTR flash_control_release(flash_control_t *ctrl)
+{
+    if (ctrl->cached_sector)
+        os_free(ctrl->cached_sector);
+}
+
+LOCAL int spiflash_read_cached_single(flash_control_t *ctrl, unsigned address, uint8_t *target, unsigned size)
+{
+    DEBUGFLASH("Request flash read at address 0x%08x size %u\n",address,size);
+
+    uint16_t sect = (address>>SECTORBITS);
+    unsigned offset = (address & ((1<<SECTORBITS)-1));
+
+    if (ctrl->cached_sector_number != sect ) {
+        spiflash_flush(ctrl);
+        ctrl->cached_sector_number = sect;
+        spi_flash_read( (unsigned)sect<<SECTORBITS, (uint32*)ctrl->cached_sector, SECTORSIZE);
+        ctrl->cached_dirty = 0;
+    }
+
+
+    os_memcpy( target, ctrl->cached_sector+offset, size);
+#if 0
+    {
+        unsigned i;
+        for (i=0;i<size;i++) {
+            os_printf("%02x ",target[i]);
+        }
+    }
+    os_printf("\n");
+#endif
+    return 0;
+}
+
+int spiflash_read_cached(flash_control_t *ctrl, unsigned address, uint8_t *target, unsigned size)
+{
+    do {
+        unsigned offset = (address & ((1<<SECTORBITS)-1));
+        unsigned toread;
+
+        if ((size+offset) > SECTORSIZE) {
+            toread = SECTORSIZE - offset;
+            DEBUGFLASH("Partial read 0x%08x size %d\n", offset, toread);
+        } else {
+            toread = size;
+        }
+        if (spiflash_read_cached_single(ctrl, address, target, toread)<0)
+            return -1;
+        address += toread;
+        size -= toread;
+        target += toread;
+    } while (size);
+    return 0;
+}
+
+int spiflash_flush(flash_control_t *ctrl)
+{
+    if (ctrl->cached_sector_number>0 && ctrl->cached_dirty) {
+        DEBUGFLASH("Writing out sector %d\n",ctrl->cached_sector_number);
+        spi_flash_write( ctrl->cached_sector_number<<SECTORBITS, (uint32*)ctrl->cached_sector, SECTORSIZE);
+        ctrl->cached_sector_number = -1;
+        ctrl->cached_dirty = 0;
+    }
+    return 0;
+}
+
+
+
+int spiflash_write_cached(flash_control_t *ctrl, unsigned address, const uint8_t *data, unsigned size)
+{
+    uint16_t sect = (address>>SECTORBITS);
+    unsigned offset = (address & ((1<<SECTORBITS)-1));
+    if (ctrl->cached_sector_number != sect ) {
+        spiflash_flush(ctrl);
+        // Read it
+/*        DEBUGFLASH("Cacheing sector %d at %08x buffer %08x\n",sect, (unsigned)sect<<SECTORBITS,
+                   (unsigned)ctrl->cached_sector
+                  );
+  */
+        spi_flash_read( (unsigned)sect<<SECTORBITS, (uint32*)ctrl->cached_sector, SECTORSIZE);
+        ctrl->cached_sector_number = sect;
+    }
+    DEBUGFLASH("Copy %08x -> %08x (%d)\n", data, ctrl->cached_sector + offset, size);
+    os_memcpy( ctrl->cached_sector + offset, data, size );
+    ctrl->cached_dirty = 1;
+    DEBUGFLASH("Cached\n");
+    
+    return 0;
+}
--- firmware/esp8266/user/flash_serializer.c
+++ firmware/esp8266/user/flash_serializer.c
@@ -5,6 +5,7 @@
 #include "alloc.h"
 #include "osapi.h"
 #include "user_interface.h"
+#include "flash.h"
 
 #define FLASH_MAGIC 0xF14F
 
@@ -24,16 +25,12 @@ typedef struct {
     uint16_t crc;
     uint8_t erased:1;
     uint8_t validated:1;
-    uint8_t cached_dirty:1;
-    int8_t cached_sector_number;
-    uint8_t *cached_sector;
+    flash_control_t fc;
 } flash_serializer_ctx_t;
 
 LOCAL flash_serializer_ctx_t flash_serializer_ctx;
 
 #define FLASHCTX(x) ((flash_serializer_ctx_t*)x->pvt)
-
-LOCAL int spiflash_read_cached(flash_serializer_ctx_t *ctx, unsigned address, uint8_t *target, unsigned size);
 
 LOCAL int flash_get_first_sector()
 {
@@ -55,14 +52,12 @@ void ICACHE_FLASH_ATTR flash_ser_initialize(struct serializer_t *me)
     me->pvt = ctx;
 
     ctx->startsector = ctx->currentsector = flash_get_first_sector();
-    ctx->cached_sector_number = -1;
-    ctx->cached_sector = os_malloc(SECTORSIZE);
     ctx->erased = 0;
     ctx->sectoroffset = sizeof(config_header_t);
     ctx->validated = 0;
     ctx->crc = 0;
-    ctx->cached_dirty = 0;
 
+    flash_control_init( &ctx->fc );
     os_printf("Start of flash area: 0x%08x\n",  ctx->startsector<<SECTORBITS);
 }
 
@@ -73,7 +68,7 @@ LOCAL int flash_validate(flash_serializer_ctx_t *ctx)
     unsigned char chunk[64];
     unsigned offset = flash_get_first_sector() << SECTORBITS;
 
-    spiflash_read_cached(ctx, offset, (void*)&header, sizeof(config_header_t));
+    spiflash_read_cached(&ctx->fc, offset, (void*)&header, sizeof(config_header_t));
     if (header.magic != FLASH_MAGIC) {
         DEBUGFLASH("Invalid flash magic %04x\n", (unsigned)header.magic);
         return -1;
@@ -93,7 +88,7 @@ LOCAL int flash_validate(flash_serializer_ctx_t *ctx)
 
     while (len) {
         unsigned size = len>64 ? 64:len;
-        spiflash_read_cached(ctx, offset, (void*)chunk, size);
+        spiflash_read_cached(&ctx->fc, offset, (void*)chunk, size);
         crc16_update_buffer(&crc, chunk, size);
         len-=size;
         offset+=size;
@@ -108,74 +103,6 @@ LOCAL int flash_validate(flash_serializer_ctx_t *ctx)
     return 0;
 }
 
-LOCAL int spiflash_flush(flash_serializer_ctx_t *ctx)
-{
-    if (ctx->cached_sector_number>0 && ctx->cached_dirty) {
-        DEBUGFLASH("Writing out sector %d\n",ctx->cached_sector_number);
-        spi_flash_write( ctx->cached_sector_number<<SECTORBITS, (uint32*)ctx->cached_sector, SECTORSIZE);
-        ctx->cached_sector_number = -1;
-        ctx->cached_dirty = 0;
-    }
-    return 0;
-}
-
-LOCAL int spiflash_read_cached_single(flash_serializer_ctx_t *ctx, unsigned address, uint8_t *target, unsigned size)
-{
-    uint16_t sect = (address>>SECTORBITS);
-    unsigned offset = (address & ((1<<SECTORBITS)-1));
-
-    if (ctx->cached_sector_number != sect ) {
-        spiflash_flush(ctx);
-        ctx->cached_sector_number = sect;
-        spi_flash_read( (unsigned)sect<<SECTORBITS, (uint32*)ctx->cached_sector, SECTORSIZE);
-        ctx->cached_dirty = 0;
-    }
-    os_memcpy( target, ctx->cached_sector+offset, size);
-    return 0;
-}
-
-
-LOCAL int spiflash_read_cached(flash_serializer_ctx_t *ctx, unsigned address, uint8_t *target, unsigned size)
-{
-    do {
-        unsigned offset = (address & ((1<<SECTORBITS)-1));
-        unsigned toread;
-
-        if ((size+offset) > SECTORSIZE) {
-            toread = SECTORSIZE - offset;
-        } else {
-            toread = size;
-        }
-        if (spiflash_read_cached_single(ctx, address, target, toread)<0)
-            return -1;
-        address += toread;
-        size -= toread;
-    } while (size);
-    return 0;
-}
-
-
-LOCAL int spiflash_write_cached(flash_serializer_ctx_t *ctx, unsigned address, const uint8_t *data, unsigned size)
-{
-    uint16_t sect = (address>>SECTORBITS);
-    unsigned offset = (address & ((1<<SECTORBITS)-1));
-    if (ctx->cached_sector_number != sect ) {
-        spiflash_flush(ctx);
-        // Read it
-        DEBUGFLASH("Cacheing sector %d at %08x buffer %08x\n",sect, (unsigned)sect<<SECTORBITS,
-                   (unsigned)ctx->cached_sector
-                  );
-
-        spi_flash_read( (unsigned)sect<<SECTORBITS, (uint32*)ctx->cached_sector, SECTORSIZE);
-        ctx->cached_sector_number = sect;
-    }
-    DEBUGFLASH("Copy %08x -> %08x (%d)\n", data, ctx->cached_sector + offset, size);
-    os_memcpy( ctx->cached_sector + offset, data, size );
-    ctx->cached_dirty = 1;
-    DEBUGFLASH("Cached\n");
-    
-    return 0;
-}
 
 
 LOCAL int flash_do_write(flash_serializer_ctx_t *ctx, const uint8_t*data, unsigned size)
@@ -195,7 +122,7 @@ LOCAL int flash_do_write(flash_serializer_ctx_t *ctx, const uint8_t*data, unsign
 
     DEBUGFLASH("Write %08x\n", (ctx->currentsector<<SECTORBITS) + ctx->sectoroffset);
 
-    if (spiflash_write_cached( ctx, (ctx->currentsector<<SECTORBITS) + ctx->sectoroffset, data, size)<0) {
+    if (spiflash_write_cached( &ctx->fc, (ctx->currentsector<<SECTORBITS) + ctx->sectoroffset, data, size)<0) {
         DEBUGFLASH("Error in write\n");
         return -1;
     }
@@ -233,7 +160,7 @@ LOCAL int ICACHE_FLASH_ATTR flash_ser_read(struct serializer_t *me, void *data, 
             return -1;
     }
 
-    int r = spiflash_read_cached(ctx,(ctx->currentsector<<SECTORBITS) + ctx->sectoroffset, data, size );
+    int r = spiflash_read_cached(&ctx->fc,(ctx->currentsector<<SECTORBITS) + ctx->sectoroffset, data, size );
     if (r>=0) {
         ctx->sectoroffset+=size;
         ctx->currentsector+= (ctx->sectoroffset & ~((1<<SECTORBITS)-1));
@@ -257,16 +184,15 @@ LOCAL void ICACHE_FLASH_ATTR flash_ser_finalise(struct serializer_t *me)
 
     header.headercrc = crc16_calc( (uint8_t*)&header, sizeof(config_header_t)-sizeof(uint16_t));
 
-    spiflash_write_cached(ctx, ctx->startsector<<SECTORBITS, (uint8_t*)&header, sizeof(header));
+    spiflash_write_cached(&ctx->fc, ctx->startsector<<SECTORBITS, (uint8_t*)&header, sizeof(header));
 
-    spiflash_flush(ctx);
+    spiflash_flush(&ctx->fc);
 }
 
 LOCAL void ICACHE_FLASH_ATTR flash_ser_release(struct serializer_t *me)
 {
-     flash_serializer_ctx_t *ctx = FLASHCTX(me);
-     if (ctx->cached_sector)
-         os_free(ctx->cached_sector);
+    flash_serializer_ctx_t *ctx = FLASHCTX(me);
+    flash_control_release(&ctx->fc);
 }
 
 
