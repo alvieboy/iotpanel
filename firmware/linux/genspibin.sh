86fd31cd60eef2c35540c5c13a7a58efe92ecfb7
Alvaro <alvieboy@alvie.com>
2016-04-06 17:36:38 +0100

    Use proper smallfs

--- firmware/linux/genspibin.sh
+++ firmware/linux/genspibin.sh
@@ -4,4 +4,4 @@
 dd if=/dev/zero of=spi.bin bs=512K count=1
 dd bs=1 if=../esp8266/firmware/0x00000.bin of=spi.bin seek=0x00000 conv=notrunc
 dd bs=1 if=../esp8266/firmware/0x40000.bin of=spi.bin seek=262144 conv=notrunc
-dd bs=1 if=../esp8266/smallfs.bin of=spi.bin seek=65536 conv=notrunc
+dd bs=1 if=../esp8266/firmware/0x10000.bin of=spi.bin seek=65536 conv=notrunc
