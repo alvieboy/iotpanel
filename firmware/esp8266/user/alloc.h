9ab97b0c55171d66b97ce89f17343837293ad155
Alvaro <alvieboy@alvie.com>
2016-04-07 18:46:09 +0100

    Add realloc

--- firmware/esp8266/user/alloc.h
+++ firmware/esp8266/user/alloc.h
@@ -3,10 +3,12 @@
 
 extern void *pvPortMalloc( size_t xWantedSize );
 extern void *pvPortCalloc( size_t xWantedSize, int iNum );
+extern void *pvPortRealloc( void *, size_t xWantedSize );
 extern void vPortFree(void *);
 
 #define os_malloc pvPortMalloc
 #define os_free vPortFree
 #define os_calloc pvPortCalloc
+#define os_realloc pvPortRealloc
 
 #endif
